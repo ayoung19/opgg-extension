06cb876f742d657db8f2f3acd6553ac31062c7d5
Andy Young <andyluyoung@gmail.com>
2024-10-08 02:21:10 -0700

    feat: use plasmo (#2)
    * feat: remove everything and set up plasmo boilerplate
    
    * chore: update package.json
    
    * chore: update prettierrc and tsconfig
    
    * feat: everything

---
+++ .prettierrc.mjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('prettier').Options}
+ */
+export default {
+  printWidth: 120,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>", // Node.js built-in modules
+    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
+    "", // Empty line
+    "^@plasmo/(.*)$",
+    "",
+    "^@plasmohq/(.*)$",
+    "",
+    "^~(.*)$",
+    "",
+    "^[./]",
+  ],
+};
